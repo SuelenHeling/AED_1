e0cc456538508dc61dcc214a3ff2fe9a5ed64305
Suelen <suelenbed@gmail.com>
2026-03-27 15:54:02 -0300

    Aula 1

---
+++ Exercicio_1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+char nome [50];
+
+printf ("Selecione uma opção:\n");
+printf ("1) Adicionar nome\n");
+printf ("2) Remover nome\n");
+printf ("3) Listar\n");
+printf ("4) Sair\n");
+
+
+
+
+}
+  
